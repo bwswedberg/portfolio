70d0dce1b863497b9f8d4d2068ec31346a120020
Brian Swedberg <bwswedberg@gmail.com>
2020-10-17 07:55:52 -0400

    Left aligned titles

--- src/sections/SectionTitle.tsx
+++ src/sections/SectionTitle.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     // borderBottom: `2px solid rgba(255,255,255,0.1)`,
     // borderTop: '2px solid rgba(255,255,255,0.1)',
     // borderRight: '2px solid rgba(255,255,255,0.1)',
-    textAlign: 'right',
+    // textAlign: 'right',
     // fontWeight: 'bold',
     // fontFamily: 'Fira Code',
     '&:before': {
